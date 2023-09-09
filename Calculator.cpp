4a61fc03764ce24c5d6493743815be506e11e1ec
Deepak Kushwaha <142688122+DeepakKush2194@users.noreply.github.com>
2023-09-10 00:30:56 +0530

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float num1,num2;
+	char operation;
+	
+	cout<<"**HeckWiz Calculator**"<<endl;
+	cin>>num1>>operation>>num2;
+	
+	switch (operation){
+		case '-':
+			cout<<num1<<operation <<num2<<"="<<num1-num2;
+			break;
+		case '+':
+			cout<<num1<<operation <<num2<<"="<<num1+num2;
+			break;
+		case '*':
+			cout<<num1<<operation <<num2<<"="<<num1*num2;
+			break;
+		case '/':
+			cout<<num1<<operation <<num2<<"="<<num1/num2;
+			break;
+		case '%':
+			bool isnum1Int, isnum2Int;
+				isnum1Int=((int)num1==num1);//5==5.0
+				isnum2Int=((int)num2==num2);
+				if(isnum1Int && isnum2Int)
+					cout<<num1<<operation <<num2<<"="<<(int)num1 % (int)num2;
+				else
+					cout<<"Not Valid!";
+					break;
+		
+	}
+	
+	
+	return 0;
+}
