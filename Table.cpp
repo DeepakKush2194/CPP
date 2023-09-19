f91d4b2641455820dd757fee91ec88d7a3f00f55
Deepak Kushwaha <142688122+DeepakKush2194@users.noreply.github.com>
2023-09-19 10:48:39 +0530

    Update Table.cpp

--- Table.cpp
+++ Table.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	
-	return 0;
+int main() {
+    int number;
+    
+    cout<< "******Table Generator*******"<<endl;
+    cout<<"Number: ";
+    cin>>number;
+    for(int i=2;i<number;i++){
+        for(int j=1;j<11;j++){
+        cout<<i<<" X "<<j<<" = "<<i*j<<endl;
+        }
+        cout<<endl;
+    }
+
+    return 0;
 }
